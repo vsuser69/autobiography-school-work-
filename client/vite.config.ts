04fc2f175e3434cad4af5f0e7287e530f71f1a38
Sasha Kapushesky <sasha.kapushesky@gmail.com>
2025-09-02 07:54:39 +0100

    added hosts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   server: {
     port: 10000,
     open: true,
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    allowedHosts: ['localhost', '127.0.0.1', '::1', '192.168.1.100', 'autobiography-school-work.onrender.com']
   }
 })
